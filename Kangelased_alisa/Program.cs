570e23c27d9f1d5a277e0b7860d55448b6edf774
opilane <opilane@HP-CZC6297B8M>
2021-09-01 10:28:49 +0300

    Add a superhero check

--- Kangelased_alisa/Program.cs
+++ Kangelased_alisa/Program.cs
@@ -6,17 +6,19 @@ namespace Kangelased_alisa
 {
     class Program
     {
-        static List<Kangelane> kangelased;
+        static List<Kangelane> kangelased = new List<Kangelane>();
         static void LaeKangelased(string failinimi)
         {
             //Console.WriteLine(File.ReadAllText(failinimi));
-            var kangelasedfailis = File.ReadAllLines(failinimi);
-            foreach(var kangelaseKirja in kangelasedfailis)
+            foreach (var kangelaseKirje in File.ReadAllLines(failinimi))
             {
-                var nimiAsukoht = kangelaseKirja.Split("/");
-                if (true)
+                var nimiAsukoht = kangelaseKirje.Split("/");
+                if (nimiAsukoht[0].EndsWith("*"))
                 {
-
+                    kangelased.Add(new Superkangelane(nimiAsukoht[0].Substring(nimiAsukoht[0].Length-1),nimiAsukoht[1]));
+                } else
+                {
+                    kangelased.Add(new Kangelane(nimiAsukoht[0],nimiAsukoht[1]));
                 }
                 Console.WriteLine(nimiAsukoht[0]);
                 Console.WriteLine(nimiAsukoht[1]);
@@ -25,13 +27,17 @@ namespace Kangelased_alisa
         }
         static void Main(string[] args)
         {
-            Superkangelane superman = new Superkangelane("Superman", "Smallville");
+            /*Superkangelane superman = new Superkangelane("Superman", "Smallville");
             int sajast = superman.Päästa(100);
             int poolsajast = superman.Päästa(50);
             Console.WriteLine(superman);
             Console.WriteLine($"Sajast:{sajast}");
-            Console.WriteLine($"Viiekümnest:{poolsajast}");
+            Console.WriteLine($"Viiekümnest:{poolsajast}");*/
             LaeKangelased("kangelased.txt");
+            foreach (var kangelane in kangelased)
+            {
+                Console.WriteLine(kangelane);
+            }
         }
 
     
